e96033a1f30e78bce9df527ac7a8637e518f1106
seyam <seyamibrahim88@gmail.com>
2024-09-15 01:13:39 +0530

    add register controller and bug fixes

--- src/db/index.js
+++ src/db/index.js
@@ -4,6 +4,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () =>{
     
+    
     try {
         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log("\n MongoDB Database connected ...")
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -44,7 +44,7 @@ const userSchema = new mongoose.Schema({
         type : String,
         required : [true, "Password is required"]
     },
-    refresToken : {
+    refreshToken : {
         type : String,
     }
 
@@ -52,7 +52,7 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password"))return next();
-    this.password = bcrypt.hash(this.password,10);
+    this.password = await bcrypt.hash(this.password,10);
     next();
 })
 
